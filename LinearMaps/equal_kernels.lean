3d58e12afafbee07f26d9022ce3a6c6e296894a0
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-02-11 19:57:55 -0600

    thm update

--- LinearMaps/equal_kernels.lean
+++ LinearMaps/equal_kernels.lean
@@ -1,7 +1,6 @@
 import Mathlib.LinearAlgebra.Dimension.Finrank
 import Mathlib.LinearAlgebra.FreeModule.Finite.Basic
-import Mathlib.LinearAlgebra.Projection
-
+import Mathlib.LinearAlgebra.FiniteDimensional.Defs
 
 /-
 Copyright (c) 2026 Miraj Samarakkody. All rights reserved.
@@ -12,12 +11,15 @@ Authors: Miraj Samarakkody
 /-!
 # Characterization of linear maps with equal kernels
 
-Let `V` is finite dimesional and `S, T ∈ L (V,W)`. Then `null S = null T` if and only if
-if there exists an invertible `E ∈ L(V)` such that `S = TE`.
+Let `V` is finite dimensional and `S, T ∈ L (V,W)`. Then `null S = null T` if and only if
+if there exists an invertible `E ∈ L(V)` such that `S = ET`.
 
 ## Main results
-
-* `LinearMap.ker_eq_ker_iff_exists_linearEquiv_comp`: The main characterization theorem.
-* `LinearMap.ker_comp_linearEquiv`: If `S = E ∘ T` for `E ∈ L(V)`, then `ker S = ker T`.
-
 -/
+
+variable {K V W : Type*} [Field K] [AddCommGroup V] [Module K V] [AddCommGroup W] [Module K W] [FiniteDimensional K V ]
+
+theorem null_eq_iff_exists_invertibles (S T : V →ₗ[K] W) :
+  LinearMap.ker S = LinearMap.ker T ↔
+  ∃ E : W ≃ₗ[K] W, S = E ∘ₗ T := by
+sorry
